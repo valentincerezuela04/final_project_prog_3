4ab4e96d87a04a4805dbf0ef6624250bec8af43c
Pablo <pablosalompita@gmail.com>
2025-05-10 18:04:55 -0300

    Proyecto base con configuración inicial y conexión a PostgreSQL

---
+++ src/main/java/com/musicreviewapp/music_review_app/MusicReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.musicreviewapp.music_review_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicreviewapp/music_review_app/MusicReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicreviewapp.music_review_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
